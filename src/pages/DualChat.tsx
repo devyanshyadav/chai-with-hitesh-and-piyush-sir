9143735d962755dbcee96e37508e020dd869c8f7
Devyansh | localhost:3000 <developerdevyansh@gmail.com>
2025-04-10 17:50:33 +0530

    added files

---
+++ src/pages/DualChat.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import ToneSlider, { ToneMode } from "./../components/ToneSlider";
+import CharacterSelector from "./../components/CharacterSelector";
+import DualChatInterface from "./../components/DualChatInterface";
+
+const DualChat = () => {
+  const [toneMode, setToneMode] = useState<ToneMode>("friend");
+  const [activeCharacters, setActiveCharacters] = useState<
+    ("hitesh" | "piyush")[]
+  >(["hitesh"]);
+
+  return (
+    <div className="max-h-screen h-screen w-full flex flex-col divide-y overflow-hidden">
+      <header className="w-full p-1 px-2 bg-slate-50 flex items-center justify-between">
+        <h1 className="text-xl font-bold flex items-center gap-2">
+          <img
+            src="/favicon.ico"
+            alt="Chai"
+            className="w-10 h-10 rounded-full"
+          />{" "}
+          Chai With Hitesh & Piyush Sir
+        </h1>
+        <button className="w-10 h-10 rounded-full flex items-center justify-center hover:shadow-lg">
+        <img width="50" height="50" src="https://img.icons8.com/3d-fluency/94/github.png" alt="github"/>
+        </button>
+      </header>
+      <main className="grid divide-x grid-cols-[300px_1fr] overflow-hidden flex-grow">
+        <div className="grid grid-rows-[auto_1fr_1fr]">
+          <ToneSlider onChange={setToneMode} />
+          <CharacterSelector onSelectionChange={setActiveCharacters} />
+        </div>
+        <DualChatInterface
+          activeCharacters={activeCharacters}
+          toneMode={toneMode}
+        />
+      </main>
+      <footer className="w-full p-1.5 *:opacity-80 bg-chai-dark text-white grid place-items-center text-center text-xs">
+        <span>
+          Made with ❤️ by{" "}
+          <a
+            href="https://devyanshyadav.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline"
+          >
+            Devyansh Developer
+          </a>
+        </span>
+      </footer>
+    </div>
+  );
+};
+
+export default DualChat;
